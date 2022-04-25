e4fe18eabfccbdf062dd88f973e1361f5b89dec2
Dinhero21 <robloxdinhero21@gmail.com>
2022-04-24 21:09:46 -0300

    Hopefully faster logger

--- logger.js
+++ logger.js
@@ -1,10 +1,40 @@
+const config = require('./config.json')
 const fs = require('fs')
 const path = require('path')
 
 class Logger {
   constructor (logPath) {
-    this.value = fs.existsSync(logPath) ? JSON.parse(fs.readFileSync(logPath)) : []
-    this.path = logPath
+    this.path = path.join(logPath, `${Date.now()}.json`)
+    this.value = []
+
+    if (!fs.existsSync(logPath)) return
+
+    let data = []
+
+    for (const filename of fs.readdirSync(logPath)) {
+      if (!/^\d+\.json$/.test(filename)) continue
+
+      try {
+        const filepath = path.join(logPath, filename)
+        const content = JSON.parse(fs.readFileSync(filepath, 'utf8'))
+
+        console.log(content)
+
+        data.push(...content)
+
+        fs.rmSync(filepath)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    const latestPath = path.join(logPath, 'latest.json')
+
+    if (fs.existsSync(latestPath)) data.push(...latestPath)
+
+    data = data.sort((a, b) => a.time - b.time)
+
+    fs.writeFileSync(latestPath, JSON.stringify(data))
   }
 
   write (data = {}) {
@@ -12,9 +42,14 @@ class Logger {
 
     const dirname = path.dirname(this.path)
 
-    if (!fs.existsSync(dirname)) fs.mkdirSync(dirname)
+    if (!fs.existsSync(dirname)) fs.mkdirSync(dirname, { recursive: true })
 
     fs.writeFileSync(this.path, JSON.stringify(this.value))
+
+    if (data.length > config.max_log_size) {
+      this.value = []
+      this.path = path.join(dirname, `${Date.now()}.json`)
+    }
   }
 
   data (type = [], content = null) {
