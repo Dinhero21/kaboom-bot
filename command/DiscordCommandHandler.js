e58c1e746e4d99a0895c48b230ea8a0236232802
Dinhero21 <robloxdinhero21@gmail.com>
2022-04-27 23:29:49 -0300

    Trust system

--- command/DiscordCommandHandler.js
+++ command/DiscordCommandHandler.js
@@ -1,14 +1,32 @@
+const config = require('../config.json')
 const CommandHandler = require('./CommandHandler.js')
 const discord = require('discord.js')
 
+function getTrust (roles) {
+  let trust = 0
+
+  for (const [id] of roles) {
+    const roleTrust = config.trust[id]
+
+    if (!roleTrust) continue
+
+    if (roleTrust > trust) trust = roleTrust
+  }
+
+  return trust
+}
+
 class DiscordCommandHandler extends CommandHandler {
-  constructor (bot, content, author, channel, args, client) {
+  constructor (bot, message, args, client) {
     super(bot)
 
-    this.content = content
-    this.author = author
-    this.channel = channel
+    this.content = message.content
+    this.author = message.author
+    this.channel = message.channel
+    this.guild = message.guild
     this.args = args
+
+    this.trust = getTrust(message.member.roles.cache)
 
     this.client = client
   }
