6d4b837c1efed2e9cd877c1c8437cf427843af71
Dinhero21 <robloxdinhero21@gmail.com>
2022-04-30 15:41:42 -0300

    No more bad handlers

--- bot.js
+++ bot.js
@@ -43,13 +43,58 @@ function createBot (options = {}) {
     z: null
   }
 
-  bot._client.on('connect', handleConnection)
-  bot._client.on('login', handleLogin)
-  bot._client.on('error', handleError)
-  bot._client.on('end', handleEnd)
-  bot._client.on('position', handlePositionChange)
-  bot._client.on('chat', handleChat)
-  bot._client.on('entity_status', handleEntityStatus)
+  bot._client.on('connect', () => {
+    bot.connected = true
+    bot.emit('connect')
+  })
+  bot._client.on('login', packet => {
+    bot.loggedIn = true
+    bot.entityId = packet.entityId
+    bot.uuid = bot._client.uuid
+
+    bot.emit('login', packet)
+
+    setInterval(tick, 1000 / 20)
+    setInterval(chatQueue, 1000 / 20 * 5)
+  })
+  bot._client.on('error', error => {
+    bot.emit('error', error)
+  })
+  bot._client.on('end', reason => {
+    bot.connected = false
+    bot.loggedIn = false
+
+    bot.emit('end', reason)
+  })
+  bot._client.on('position', position => {
+    bot.position = position
+
+    bot.emit('position', position)
+  })
+  bot._client.on('chat', packet => {
+    bot.emit('chat', packet)
+  })
+  bot._client.on('entity_status', packet => {
+    if (packet.entityId === bot.entityId) {
+      switch (packet.entityStatus) {
+        case 24:
+          bot.op = false
+
+          bot.emit('deop')
+          break
+        case 28:
+          bot.op = true
+
+          bot.emit('op')
+          break
+        default:
+          console.log(`Entity status: ${packet.entityStatus}`)
+          break
+      }
+    }
+
+    bot.emit('entity_status', packet)
+  })
 
   bot.on('parsed_chat', data => {
     const filters = {
@@ -76,58 +121,6 @@ function createBot (options = {}) {
 
   return bot
 
-  function handleConnection () {
-    bot.connected = true
-    bot.emit('connect')
-  }
-  function handleLogin (packet) {
-    bot.loggedIn = true
-    bot.entityId = packet.entityId
-    bot.uuid = bot._client.uuid
-
-    bot.emit('login', packet)
-
-    setInterval(tick, 1000 / 20)
-    setInterval(chatQueue, 1000 / 20 * 5)
-  }
-  function handleError (error) {
-    bot.emit('error', error)
-  }
-  function handleEnd (reason) {
-    bot.connected = false
-    bot.loggedIn = false
-
-    bot.emit('end', reason)
-  }
-  function handlePositionChange (position) {
-    bot.position = position
-
-    bot.emit('position', position)
-  }
-  function handleChat (packet) {
-    bot.emit('chat', packet)
-  }
-  function handleEntityStatus (packet) {
-    if (packet.entityId === bot.entityId) {
-      switch (packet.entityStatus) {
-        case 24:
-          bot.op = false
-
-          bot.emit('deop')
-          break
-        case 28:
-          bot.op = true
-
-          bot.emit('op')
-          break
-        default:
-          console.log(`Entity status: ${packet.entityStatus}`)
-          break
-      }
-    }
-
-    bot.emit('entity_status', packet)
-  }
   function chatQueue () {
     const message = bot.chatQueue.shift()
 
